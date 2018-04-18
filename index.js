8ed024409df2514b8a7b5b696ebda344569e9a7b
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-18 00:48:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function theBeatlesPlay(array_of_musicians, array_of_instruments)
 {
+  var empty_array = new Array()
 
-  
+  for(var i = 0; i < array_of_musicians.length; i++)
+  {
+    empty_array.push(`${array_of_musicians[i] plays array_of_instruments[i]}`)
+  }
+
+  return empty_array
 }
